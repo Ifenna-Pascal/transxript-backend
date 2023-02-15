5a79984b3d1f1296f0ccddf1d93dd3013769c1c2
ifenna-pascal <ifennamonanu70@gmail.com>
2023-02-15 07:07:35 +0100

    add

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: './.env' });
 
-export const CORS_ORIGIN = process.env.CORS_ORIGIN;
+export const CORS_ORIGIN = process.env.CORS_ORIGIN || 'http://localhost:3000';
 export const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING || '';
 export const JWT_SECRET = process.env.JWT_SECRET || 'new';
 export const APP_NAME = process.env.APP_NAME || '';
